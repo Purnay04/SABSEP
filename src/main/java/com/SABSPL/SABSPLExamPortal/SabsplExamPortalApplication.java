0adf83d728e81729a47a9ddc7c9cec497c5202b4
Purnay Barge <bargeamanpurnay087@gmail.com>
2024-02-15 01:47:14 +0530

    committing basic backend strutucture with basic db configuration

---
+++ src/main/java/com/SABSPL/SABSPLExamPortal/SabsplExamPortalApplication.java
@@ -0,0 +1,13 @@
+package com.SABSPL.SABSPLExamPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabsplExamPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabsplExamPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SABSPL/SABSPLExamPortal/SabsplExamPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SABSPL.SABSPLExamPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabsplExamPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
